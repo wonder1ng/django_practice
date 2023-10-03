e738553b9916c21e092bcafc72ec166ade3017b6
wonder1ng <wonder1ng@velog.io>
2023-10-03 16:33:16 +0900

    second commit

--- mysite/accountapp/views.py
+++ mysite/accountapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def hello_world(request):
+#     return HttpResponse('Hello world!')
+
+# def hello_world(request):
+#     return render(request, 'base.html')
+
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'accountapp/hello_world.html')
