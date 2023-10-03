20633185fa96aac12970ba754b12e52ff09efafc
wonder1ng <wonder1ng@velog.io>
2023-10-03 15:59:07 +0900

    fist commit

---
+++ mysite/accountapp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountapp.views import hello_world
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello'),
+]
